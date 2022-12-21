cb728b89767cee163b35d11b260489fe957d7c26
Cendyne <cendyne@cendyne.dev>
2022-12-21 12:31:14 -0600

    Add Ibzan

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export interface Env {
+
 }
 
 function webFingerMastodon(host: string, username: string) : any {
@@ -84,62 +85,66 @@ interface WebFinger {
 import untypedData from './data.json'
 const webFingerTable : WebFinger = untypedData as any;
 
+function webFinger(request: Request) : Response {
+  let {hostname, searchParams} = new URL(request.url);
+  const resource = searchParams.get('resource');
+  if (resource) {
+    let match = resource.toLocaleLowerCase().match(/^acct:([^@]+)@(.+)$/);
+    let webFinger : any | undefined;
+    if (match) {
+      let [,fingerUsername,fingerHost] = match;
+      if (fingerHost == hostname) {
+        const webFingerData = webFingerTable[fingerUsername];
+        if (webFingerData) {
+          switch (webFingerData[0]) {
+            case 'mastodon': {
+              webFinger = webFingerMastodon(webFingerData[1], webFingerData[2]);
+              break;
+            }
+            case 'pleroma':
+            case 'akkoma': {
+              webFinger = webFingerAkkomaOrPleroma(webFingerData[1], webFingerData[2]);
+              break;
+            }
+            case 'misskey': {
+              webFinger = webFingerMisskey(webFingerData[1], webFingerData[2]);
+              break;
+            }
+            default: {
+              console.log(`Unknown webFinger type ${webFingerData[0]}`)
+            }
+          }
+        } else {
+          console.log(`${fingerUsername} not known`);
+        }
+      } else {
+        console.log(`${fingerHost} not matching`);
+      }
+    } else {
+      console.log(`Webfinger resource ${resource} not supported`)
+    }
+    if (webFinger) {
+      return new Response(JSON.stringify(webFinger), {
+        headers: {
+        'content-type': 'application/jrd+json'
+      }})
+    }
+  }
+  return new Response('Not Found', {
+    status: 404
+  });
+}
+
 export default {
 	async fetch(
 		request: Request,
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-
 		if (request.method == 'GET') {
-			let {hostname, pathname, searchParams} = new URL(request.url);
+			let {pathname} = new URL(request.url);
 			if (pathname == '/.well-known/webfinger') {
-				const resource = searchParams.get('resource');
-				if (resource) {
-					let match = resource.toLocaleLowerCase().match(/^acct:([^@]+)@(.+)$/);
-					let webFinger : any | undefined;
-					if (match) {
-						let [,fingerUsername,fingerHost] = match;
-						if (fingerHost == hostname) {
-							const webFingerData = webFingerTable[fingerUsername];
-							if (webFingerData) {
-								switch (webFingerData[0]) {
-									case 'mastodon': {
-										webFinger = webFingerMastodon(webFingerData[1], webFingerData[2]);
-										break;
-									}
-									case 'pleroma':
-									case 'akkoma': {
-										webFinger = webFingerAkkomaOrPleroma(webFingerData[1], webFingerData[2]);
-										break;
-									}
-									case 'misskey': {
-										webFinger = webFingerMisskey(webFingerData[1], webFingerData[2]);
-										break;
-									}
-									default: {
-										console.log(`Unknown webFinger type ${webFingerData[0]}`)
-									}
-								}
-							} else {
-								console.log(`${fingerUsername} not known`);
-							}
-						} else {
-							console.log(`${fingerHost} not matching`);
-						}
-					} else {
-						console.log(`Webfinger resource ${resource} not supported`)
-					}
-					if (webFinger) {
-						return new Response(JSON.stringify(webFinger), {
-							headers: {
-							'content-type': 'application/jrd+json'
-						}})
-					}
-				}
-				return new Response('Not Found', {
-					status: 404
-				});
+				return webFinger(request)
 			}
 		}
 
